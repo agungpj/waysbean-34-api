db9d1b4575d8862fe5f6250fe7331d134c6bd695
Agung Prasetya <agungprasetya1121@gmail.com>
2022-06-28 07:49:49 +0700

    deploy

---
+++ seeders/20220628004043-topping.js
@@ -0,0 +1,37 @@
+"use strict";
+const imgsample = require("../uploads/imgsample.png");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "tb_toppings",
+      [
+        {
+          title: "toppingsample",
+          price: 20000, //123456
+          image: imgsample,
+          idUser: 1,
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
